5b12bc31ece31d9b2648e96f7027859f6d1c8a11
benyasza <akos.benyasz@possible.com>
2016-08-15 13:42:43 +0200

    initial commit

---
+++ Sitecore/MongoRolesInRolesProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Sitecore.Security.Accounts;
+
+namespace Sitecore.CustomMongoProviders
+{
+    public class MongoRolesInRolesProvider : RolesInRolesProvider
+    {
+        public override void AddRolesToRoles(IEnumerable<Role> memberRoles, IEnumerable<Role> targetRoles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveRoleRelations(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveRolesFromRoles(IEnumerable<Role> memberRoles, IEnumerable<Role> targetRoles)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<Role> FindRolesInRole(string targetRoleName, string roleNameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<Role> GetRolesForRole(string memberRoleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<Role> GetRolesInRole(string targetRoleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsRoleInRole(string memberRoleName, string targetRoleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
